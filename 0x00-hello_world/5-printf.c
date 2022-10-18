318851fd30fa712ff1494fc0e2b0c60c163a0439
Victoria Agbenorku <Victoriaagbenorku@yahoo.com>
2022-10-17 17:51:12 -0700

    I

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
 
-int main(void)
+
+int main(void) /* main - print out text and next line */
 
 {
 
