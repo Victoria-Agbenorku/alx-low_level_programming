e4e16a85a674f5d5aa6c194c13a2a129a1d0cfb0
Victoria <113602701+Victoria-Agbenorku@users.noreply.github.com>
2022-10-18 01:20:15 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) /* main - print out text and next line */
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
