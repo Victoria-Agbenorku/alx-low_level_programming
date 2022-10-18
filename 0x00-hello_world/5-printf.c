88a13489380c0323e6231fbacebe13bec437b2ef
Victoria <113602701+Victoria-Agbenorku@users.noreply.github.com>
2022-10-18 01:13:40 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-
-
 int main(void) /* main - print out text and next line */
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
