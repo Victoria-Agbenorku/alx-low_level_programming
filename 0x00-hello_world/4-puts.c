fb54a2da3896af18f3727635988a10110c8381d3
Victoria Agbenorku <Victoriaagbenorku@yahoo.com>
2022-10-17 17:36:00 -0700

    Task 2 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/*
+ *
+ *  * main - prints "and that piece of art is useful" Dora Korpar 2015-10-19",
+ *
+ *   * followed by a new line, to standard erro. * * 
+ *
+ *    * Return: Always 1
+ *
+ *     */
+
+int main(void)
+
+{
+
+		write("and that piece of art is useful\n" - Dora_Korpar, 2015-10-19\n",
+
+							59);
+
+			return (1);
+
+}
