f1a1614e90b0eb4adb0572d3690ce9d2c4d78275
Victoria <113602701+Victoria-Agbenorku@users.noreply.github.com>
2022-10-18 01:17:10 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /*
- *
- *  * Prints Programming is like building multilingual puzzle,
- *
- *   * followed by a new line
- *
- *    */
-
+ * Prints Programming is like building multilingual puzzle,
+ * followed by a new line
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
 }
