99bbf1263b40528a56184eef0801663ba5428348
Victoria Agbenorku <Victoriaagbenorku@yahoo.com>
2022-10-17 18:02:42 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,28 @@
-#!/bin/bash
 #include <stdio.h>
 
-/*
+/**
  *
- *  * main - Prints the size of variable types on machine   
+ *  * main - Entry point
  *
- *   */
+ *   *
+ *
+ *    * Return: always 0
+ *
+ *     */
 
 int main(void)
 
 {
 
-		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-			printf("Size of an int: %zu byte(s)\n", sizeof(int));
+			printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-						printf("Size of a float %zu byte(s)\n", sizeof(float));
+						printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 							return (0);
 
